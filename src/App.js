943d5e598391174f24eb658459fc36f67e440181
Adrian <adriangiulianotucci@gmail.com>
2020-09-02 19:46:23 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ProductGrid from './components/ProductGrid';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, CardHeader
-} from 'reactstrap';
 import imgPanel from './header-x2.png'
 
 function App() {
   return (
-    <Card>
-        <CardHeader>Header</CardHeader>
-        <CardImg top width="100%" src={imgPanel} alt="Card image cap" />
-        <CardBody>
+    <div>
+        <header>Header</header>
+        <img width="100%" src={imgPanel} alt="Card image cap" />
+        <div className='navBar'>
+          <div>
+            <div className='navButton'>16 of 32 products</div>
+            <div className='navButton'>Sort by</div>
+            <div className='navButton'>Most Recent</div>
+            <div className='navButton'>Lowest Price</div>
+            <div className='navButton'>Highest Price</div>
+          </div>
+          <div>
+            <div className='navArrow'>Flecha</div>
+          </div>
+        </div>
+        <div>
           <ProductGrid> </ProductGrid>
-        </CardBody>
-      </Card>
+        </div>
+      </div>
   );
 }
 
