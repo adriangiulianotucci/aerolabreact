166ad9895e7490bed0794638dbb79254ac65ad96
Adrian <adriangiulianotucci@gmail.com>
2020-10-12 19:37:57 -0300

    css

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,33 +1,14 @@
 import React, { useState, useEffect , useContext } from 'react';
 import './Product.css'
 import { UserContext } from '../context/context'
+import api from '../utils/api'
 
-function ProductPrice(props) {
-    return (
-          <div className='priceHover'>
-            <img src={'/assets/icons/buy-white.svg'} className='buyIcon'/>
-            <div className='price'>
-              <h1>{props.cost}</h1>
-              <img src='/assets/icons/coin.svg'></img>
-              <div className='redeemButton'>
-                <h2>Redeem now</h2>
-              </div>
-            </div>
-          </div>
-        ) 
-}
-
-function PriceWarning(props) {
-  return (
-    <div className='buyIcon priceWarning'>You need {props.cost}<img src='/assets/icons/coin.svg'></img></div>
-    ) 
-}
 
 function Product(props) { 
   const user = useContext(UserContext)
   const [available, setAvailable] = useState(false)
   const [hovered, setHovered] = useState(false)
-
+  
   useEffect(() => {
     if (!user) {
       setAvailable(false)
@@ -38,22 +19,48 @@ function Product(props) {
   },[user]);
   
     return (
-
-          <div className='product' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-            {hovered && available && <ProductPrice cost={props.product.cost}></ProductPrice>}
-            <div className='upperCard'>
-              <img width="100%" src={props.product.img.url} alt="Card image cap" className='productImg'/>
-              {!hovered && available && <img src={'/assets/icons/buy-blue.svg'} className='buyIcon'/>}    
-              {!available && <PriceWarning cost={props.product.cost}></PriceWarning>}     
+      
+            <div className='product' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+              {hovered && available && <ProductPrice product={props.product}></ProductPrice>}
+              <div className='upperCard'>
+                <img width="100%" src={props.product.img.url} alt="Card image cap" className='productImg'/>
+                {!hovered && available && <img src={'/assets/icons/buy-blue.svg'} className='buyIcon'/>}    
+                {!available && <PriceTag product={props.product}></PriceTag>}     
+              </div>
+              <div className='lowerCard'>
+                <h2>{props.product.category}</h2>
+                <h1>{props.product.name}</h1>
+              </div>
             </div>
-            <div className='lowerCard'>
-              <h2>{props.product.category}</h2>
-              <h1>{props.product.name}</h1>
+            ) 
+}
+
+function ProductPrice(props) {
+
+  const redeemProduct = function(props) {
+    api.redeemProduct(props.product._id);
+    window.location.reload(false);
+  }
+
+    return (
+          <div className='priceHover' onClick={()=> redeemProduct(props)}>
+            <img src={'/assets/icons/buy-white.svg'} className='buyIcon'/>
+            <div className='price'>
+              <h1>{props.product.cost}</h1>
+              <img src='/assets/icons/coin.svg'></img>
+              <div className='redeemButton'>
+                <h2>Redeem now</h2>
+              </div>
             </div>
           </div>
-
-          
         ) 
 }
 
-  export default Product
+function PriceTag(props) {
+  return (
+    <div className='buyIcon priceTag'>You need {props.product.cost}<img src='/assets/icons/coin.svg'></img></div>
+    ) 
+}
+
+
+export default Product
